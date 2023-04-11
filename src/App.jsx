489d5541987898678f12638871728df948f0c25b
seriokim <atomyoun@gmail.com>
2023-04-11 21:31:53 +0900

    [23.04.11]  헤더 + firebase 로그인인

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./component/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { FiShoppingBag } from "react-icons/fi";
+import { BsFillPencilFill } from "react-icons/bs";
+import { login, logout, onUserStateChange } from "./../api/firebase";
+import User from "./User";
+import Button from "./ui/Button";
+
+export default function Navbar() {
+  const [user, setUser] = useState();
+  useEffect(() => {
+    onUserStateChange((user) => {
+      setUser(user);
+    });
+  }, []);
+
+  return (
+    <header className="flex justify-between border-b border-gray-300 p-3">
+      <Link to="/" className="flex items-center text-4xl text-brand">
+        <FiShoppingBag />
+        <h1>Shoppy</h1>
+      </Link>
+      <nav className="flex items-center gap-4 font-semibold">
+        <Link to="/products">Products</Link>
+        {user && <Link to="/carts">Carts</Link>}
+        {user && user.isAdmin && (
+          <Link to="/products/new" className="text-xl">
+            <BsFillPencilFill />
+          </Link>
+        )}
+        {user && <User user={user} />}
+        {!user && <Button text={"Login"} onClick={login} />}
+        {user && <Button text={"Logout"} onClick={logout} />}
+      </nav>
+    </header>
+  );
+}
